341a32ba79f11e4558257fccfa423a5b2bf99df3
Nicole Saß <sassnicole6@gmail.com>
2023-10-05 15:50:24 +0200

    site trasfert

--- main.js
+++ main.js
@@ -15,26 +15,26 @@ document.querySelector('#app').innerHTML = /*html */`
     <nav id="hamburger-nav">
         <div class="logo">logo</div>
         <div class="hamburger-menu">
-            <div class="hamburger-icon" onclick="toggleMenu()">
+            <div class="hamburger-icon">
                 <span></span>
                 <span></span>
                 <span></span>
             </div>
             <div class="menu-links">
-                <li><a href="#about" onclick="toggleMenu()">About</a></li>
-                <li><a href="#projects" onclick="toggleMenu()" >Projects</a></li>
-                <li><a href="#contact" onclick="toggleMenu()">Contact</a></li>
+                <li><a href="#about" >About</a></li>
+                <li><a href="#projects"  >Projects</a></li>
+                <li><a href="#contact">Contact</a></li>
             </div>
         </div>
     </nav>
 
 
 `;
-
-function toggleMenu() {
+const hamburgerIconElem = document.querySelector(".hamburger-icon")
+hamburgerIconElem.addEventListener("click", ()=>{
   const menu = document.querySelector(".menu-links");
   const icon = document.querySelector(".hamburger-icon");    
   menu.classList.toggle("open");
   icon.classList.toggle("open");
-}
+})
 
