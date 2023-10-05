103349006cfbfd67a7239eb21cc4d9144c51403e
Nicole Saß <sassnicole6@gmail.com>
2023-10-05 15:35:07 +0200

    old site is in

--- main.js
+++ main.js
@@ -29,5 +29,12 @@ document.querySelector('#app').innerHTML = /*html */`
     </nav>
 
 
-`
+`;
+
+function toggleMenu() {
+  const menu = document.querySelector(".menu-links");
+  const icon = document.querySelector(".hamburger-icon");    
+  menu.classList.toggle("open");
+  icon.classList.toggle("open");
+}
 
